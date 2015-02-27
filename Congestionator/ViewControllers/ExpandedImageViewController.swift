cf20d55e958299e1f90c7c8c56e688656f09e06c
Dean Woodward <dean.woodward@datacom.co.nz>
2015-02-27 22:53:01 +1300

    Automaticlly load images on timer.

--- Congestionator/ViewControllers/ExpandedImageViewController.swift
+++ Congestionator/ViewControllers/ExpandedImageViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
+var expandedImageViewController: ExpandedImageViewController!
+
 class ExpandedImageViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var progressView: UIProgressView!
-    
     
     var congestionLocation: CongestionLocation!
     var currentIndex = 0
@@ -21,18 +22,26 @@ class ExpandedImageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView.sd_setImageWithURL(NSURL(string: congestionLocation.imageUrl!), placeholderImage: nil, options: SDWebImageOptions.RefreshCached)
+        expandedImageViewController = self
+        //imageView.sd_setImageWithURL(NSURL(string: congestionLocation.imageUrl!), placeholderImage: nil, options: SDWebImageOptions.RefreshCached)
+        loadImage()
         navigationItem.title = congestionLocation.title
         
         let transform = CGAffineTransformMakeScale(1.0, 3.0)
         progressView.transform = transform
         progressView.setProgress(0.0, animated: false)
+        
     }
     
 //    override func viewWillAppear(animated: Bool) {
 //        super.viewWillAppear(animated)
 //        navigationController?.navigationBar.layer.removeAllAnimations()
 //    }
+
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer?.invalidate()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -45,22 +54,32 @@ class ExpandedImageViewController: UIViewController {
     @IBAction func closeTapped(sender: UIBarButtonItem) {
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    func loadImage() {
+        let block: SDWebImageCompletionBlock! = {(image: UIImage!, error: NSError!, cacheType: SDImageCacheType!, imageURL: NSURL!) -> () in
+                if cacheType == .None {
+                    println("Downloaed from the web")
+                    expandedImageViewController.congestionLocation.storedImages.append(expandedImageViewController.imageView.image!)
+                    expandedImageViewController.currentIndex = expandedImageViewController.congestionLocation.storedImages.count - 1
+                }
+            }
+        let url = NSURL(string: congestionLocation.imageUrl!)
+        imageView.sd_setImageWithURL(url, placeholderImage: nil, options: .RefreshCached, completed: block)
+    }
 
     @IBAction func refresh(sender: UIBarButtonItem) {
-        imageView.sd_setImageWithURL(NSURL(string: congestionLocation.imageUrl!), placeholderImage: nil, options: SDWebImageOptions.RefreshCached)
-        congestionLocation.storedImages.append(imageView.image!)
-        currentIndex = congestionLocation.storedImages.count - 1
+        loadImage()
     }
 
     @IBAction func forwardTapped(sender: UIBarButtonItem) {
-        if congestionLocation.storedImages.count > 0 {
+        if congestionLocation.storedImages.count > 1 {
             playing = !playing
             if playing {
                 sender.image = UIImage(named: "Stop")
                 timer = NSTimer.scheduledTimerWithTimeInterval(0.4, target: self, selector: Selector("play"), userInfo: nil, repeats: true)
             } else {
                 timer?.invalidate()
-                sender.image = UIImage(named: "ArrowRight")
+                sender.image = UIImage(named: "Play")
             }
         }
     }
@@ -72,15 +91,15 @@ class ExpandedImageViewController: UIViewController {
         println("Animating")
     }
     
-    @IBAction func backTapped(sender: UIBarButtonItem) {
-        if congestionLocation.storedImages.count > 0 {
-            --currentIndex
-            if currentIndex < 0 {
-                currentIndex = congestionLocation.storedImages.count - 1
-            }
-            imageView.image = congestionLocation.storedImages[currentIndex]
-        }
-    }
+//    @IBAction func backTapped(sender: UIBarButtonItem) {
+//        if congestionLocation.storedImages.count > 0 {
+//            --currentIndex
+//            if currentIndex < 0 {
+//                currentIndex = congestionLocation.storedImages.count - 1
+//            }
+//            imageView.image = congestionLocation.storedImages[currentIndex]
+//        }
+//    }
     
     
     
--- Congestionator/ViewControllers/MapViewController.swift
+++ Congestionator/ViewControllers/MapViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 import MapKit
 
+var mapViewController: MapViewController!
+
 class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     private var congestionLocations = [CongestionLocation]()
+    
+    var dummyImageView: UIImageView!
+    var timer: NSTimer?
+    var counter = 0
     
     // MARK: - View Controller Life Cycle
     
@@ -22,12 +28,25 @@ class MapViewController: UIViewController {
         mapView.showsUserLocation = true
         mapView.delegate = self
         loadCongestionLocations()
+        
+        dummyImageView = UIImageView()
+        dummyImageView.hidden = true
+        view.addSubview(dummyImageView)
+        
+        mapViewController = self
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: Selector("loadImages"), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+//    override func viewDidAppear(animated: Bool) {
+//        super.viewDidAppear(animated)
+//        timer = NSTimer.scheduledTimerWithTimeInterval(15, target: self, selector: Selector("loadImages"), userInfo: nil, repeats: true)
+//    }
     
     // MARK: - Congestion Location data
     
@@ -68,6 +87,37 @@ class MapViewController: UIViewController {
         var expandedView = navController.childViewControllers.first! as ExpandedImageViewController
         expandedView.setCongestionLocation(congestionLocation)
         presentViewController(navController, animated: true, completion: nil)
+    }
+    
+    func loadImages() {
+        println("In load images")
+        let block: SDWebImageCompletionBlock! = {(image: UIImage!, error: NSError!, cacheType: SDImageCacheType!, imageURL: NSURL!) -> () in
+            println("In block")
+                if cacheType == .None {
+                    println("Downloaed from the web")
+//                    var bob = mapViewController.congestionLocations.filter(){
+//                        return $0.imageUrl! == imageURL
+//                    }
+                    var jim = mapViewController.congestionLocations[mapViewController.counter]
+                    //jim.storedImages.append(mapViewController.dummyImageView.image!)
+                    jim.storedImages.append(image)
+                    mapViewController.counter = ++mapViewController.counter % (mapViewController.congestionLocations.count - 1)
+                }
+                //mapViewController.counter = ++mapViewController.counter % (mapViewController.congestionLocations.count - 1)
+            }
+        //let url = NSURL(string: congestionLocation.imageUrl!)
+        if !congestionLocations.isEmpty {
+//            for congestionLocation in congestionLocations {
+//                let url = NSURL(string: congestionLocation.imageUrl!)
+//                println("URL: \(url)")
+//                dummyImageView.sd_setImageWithURL(url, placeholderImage: nil, options: .RefreshCached, completed: block)
+//            }
+            var alfred = congestionLocations[counter]
+            let url = NSURL(string: alfred.imageUrl!)
+            println("URL: \(url)")
+            dummyImageView.sd_setImageWithURL(url, placeholderImage: nil, options: .RefreshCached, completed: block)
+            
+        }
     }
 }
 
